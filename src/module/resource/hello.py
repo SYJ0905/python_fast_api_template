dacb74a8b6fa94f70a64e655e94ed0f9889453e1
Cloud <BetaPhoenixSNSD@gmail.com>
2023-08-15 07:30:12 +0800

    feat.learn fast api parameters

--- src/module/resource/hello.py
+++ src/module/resource/hello.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+from pydantic import BaseModel
 
 router = APIRouter()
 
@@ -9,4 +10,37 @@ def read_root():
         "code": "1",
         "data": None,
         "message": "Hello World",
+    }
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@router.put("/test/{item_id}")
+async def create_item(
+    item_id: int,
+    item: Item,
+    needy: str,
+    limit: int | None = 10,
+    q: str
+    | None = Query(
+        default=None,
+        title="Query title",
+        description="Query description",
+        deprecated=False,
+    ),
+    # q: str | None = Query(min_length=3, max_length=10),
+    # q: str | None = Query(default=None, min_length=3, max_length=10),
+):
+    return {
+        "item_id": item_id,
+        "item": item.model_dump(),
+        "limit": limit,
+        "needy": needy,
+        "q": q,
+        **item.model_dump(),
     }
